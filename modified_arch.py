98606477f0d0b8fb6a464fd1eb69d7cfd71262a9
Mihir Jain <48515930+mihircjain@users.noreply.github.com>
2020-02-15 23:52:51 -0800

    changed layers

--- modified_arch.py
+++ modified_arch.py
@@ -19,13 +19,15 @@ class FCN(nn.Module):
         self.relu    = nn.ReLU(inplace=True)
        
         self.deconv1 = nn.ConvTranspose2d(256, 256, kernel_size=3, stride=2, padding=1, dilation = 1,  output_padding=1)
-        self.bn1     = nn.BatchNorm2d(256)        
-        self.deconv2 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation = 1 , output_padding=1)
-        self.bn2     = nn.BatchNorm2d(128)        
-        self.deconv3 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1,  output_padding=1)
-        self.bn3     = nn.BatchNorm2d(64)
-        self.deconv4 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.bn4     = nn.BatchNorm2d(32)       
+        self.bn1     = nn.BatchNorm2d(256) 
+        self.deconv2 = nn.ConvTranspose2d(256, 256, kernel_size=3, stride=2, padding=1, dilation = 1,  output_padding=1)
+        self.bn2     = nn.BatchNorm2d(256)        
+        self.deconv3 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation = 1 , output_padding=1)
+        self.bn3     = nn.BatchNorm2d(128)        
+        self.deconv4 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1,  output_padding=1)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.bn5     = nn.BatchNorm2d(64)       
         
         self.classifier = nn.Conv2d(32, 34, kernel_size=1)
 
@@ -42,8 +44,9 @@ class FCN(nn.Module):
         # Complete the forward function for the rest of the decoder
         x = self.bn2(self.relu(self.deconv2(x)))
         x = self.bn3(self.relu(self.deconv3(x)))
+        x = self.bn4(self.relu(self.deconv4(x)))
                      
-        out_decoder = self.bn4(self.relu(self.deconv4(x)))
+        out_decoder = self.bn5(self.relu(self.deconv5(x)))
         score = self.classifier(out_decoder)                   
         
         # ***** might have to include softmax
